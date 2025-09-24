b0fcf3f2e18f71ee61e0d03fde143e283406bc4a
João Vitor Soares <joaovitorsoares12@gmail.com>
2025-09-24 20:53:23 -0300

    feat: Integrate @prisma/nextjs-monorepo-workaround-plugin for improved Prisma support

--- apps/sendo-base/next.config.mjs
+++ apps/sendo-base/next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 const nextConfig = {
   transpilePackages: ["@base-church/ui", "@base-church/db", "@repo/icons"],
   images: {
@@ -15,6 +16,12 @@ const nextConfig = {
         port: "",
       },
     ],
+  },
+  webpack: (config, { isServer }) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+    if (isServer) config.plugins = [...config.plugins, new PrismaPlugin()];
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return config;
   },
 };
 
